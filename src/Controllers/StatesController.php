b7b6895df318cda78e8b7d9eca0eb73bbabd39e8
Nick Rupert <nickrupert7@gmail.com>
2020-11-23 14:54:48 -0500

    Updated states GET

--- src/Controllers/StatesController.php
+++ src/Controllers/StatesController.php
@@ -9,12 +9,12 @@ use Illuminate\Routing\Controller;
 class StatesController extends Controller
 {
     public function index(Request $request) {
-        $states = State::all();
+        $states = State::query();
 
         if ($request->has('country_code')) {
             $states = $states->where('country_code', $request->get('country_code'));
         }
 
-        return $states;
+        return $states->get();
     }
 }
